2547334ed9ffc8f04bbcb63c3e86a3db985a5732
Kevin Klions <kklions1@gmail.com>
2023-11-27 09:26:30 -0500

    added main loop, updated print function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
-const GRID_SIZE: usize = 7; 
+const GRID_SIZE: usize = 19; 
 pub type Grid = [[bool; GRID_SIZE]; GRID_SIZE];
 
+use std::{thread, time}; 
+
+
 fn print_grid(grid: &Grid) { // TODO would this be better as a macro?
+    println!("----------------------------");
     for row in grid { 
         for col in row { 
-            print!("{}", *col as u8);
+            if *col { 
+                print!("\u{2588}"); 
+            } else { 
+                print!("\u{2591}");
+            }
         }
         print!("\n");
     }
     print!("\n");
+}
+
+fn init_grid() -> Grid { 
+    let grid = [
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, true, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false],
+        [false, false, true, true, true, false, false, false, false, true, true, true, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, true, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+    ];
+
+    grid
 }
 
 fn alive_neighbor_count(grid: &Grid, point: (usize, usize)) -> u8 {
@@ -34,14 +68,14 @@ fn calculate_next_step(grid: &Grid) -> Grid {
             let neighbor_count = alive_neighbor_count(&grid, (row, col));
             let current_cell = grid[row][col];
 
-            if (current_cell) { // Live cells
-                if (neighbor_count == 2 || neighbor_count == 3) { 
+            if current_cell { // Live cells
+                if neighbor_count == 2 || neighbor_count == 3 { 
                     copy_grid[row][col] = true;
                 } else { 
                     copy_grid[row][col] = false;
                 }
             } else { // Dead cells  
-                if (neighbor_count == 3) { 
+                if neighbor_count == 3 { 
                     copy_grid[row][col] = true;
                 } else { 
                     copy_grid[row][col] = false;
@@ -55,8 +89,12 @@ fn calculate_next_step(grid: &Grid) -> Grid {
 
 
 fn main() {
-    let mut grid = [[false; GRID_SIZE]; GRID_SIZE];
-    print_grid(&grid);
-    let nextGrid = calculate_next_step(&grid);
-    print_grid(&nextGrid);
+    let mut current_grid: Grid = init_grid();
+    print_grid(&current_grid); 
+
+    loop { 
+        current_grid = calculate_next_step(&current_grid); 
+        print_grid(&current_grid); 
+        thread::sleep(time::Duration::from_millis(1000));
+    }
 }
